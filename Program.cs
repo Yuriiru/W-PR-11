1c1c05614340c763591498d254f14b00f57615f1
Yuriiru <98612792+Yuriiru@users.noreply.github.com>
2023-04-29 11:51:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+    internal class Program
+    {
+        delegate string WeekDaysDelegate();
+        static void Main(string[] args)
+        {
+            string[] WeekDays = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+            int index = -1;
+
+            WeekDaysDelegate weekDaysDelegate = () =>
+            {
+                index = (index + 1) % 7;
+                return WeekDays[index];
+            };
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine(weekDaysDelegate());
+            }
+
+            Console.ReadLine();
+        }
+    }
+
